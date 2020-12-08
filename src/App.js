ee08dd85865d6549efd8fb021689891d497f86a7
Kerry-Cho <cdh0012@naver.com>
2020-12-08 14:30:32 +0900

    class component type으로 변경함

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    currenTime: Date(),
+  };
+
+  timer;
+
+  constructor(Props) {
+    super(Props);
+
+    this.handleUpdateTimer = this.handleUpdateTimer.bind(this);
+  }
+
+  handleUpdateTimer() {
+    this.timer = setTimeout(this.handleUpdateTimer, 1000);
+
+    fetch("/api/time").then(async (response) => {
+      const data = await response.json();
+      this.setState({
+        currenTime: new Date(data.now),
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.timer = setTimeout(this.handleUpdateTimer, 1000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.state.currenTime.toLocaleString()}
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
